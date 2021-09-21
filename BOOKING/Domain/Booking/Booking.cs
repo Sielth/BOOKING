0df02d02cbb822883c932c4dd26284df19f273e3
Sielth <sielth@hotmail.it>
2021-09-21 10:26:23 +0200

    booking added

---
+++ BOOKING/Domain/Booking/Booking.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOOKING.Domain.Booking
+{
+    public class Booking : IBooking
+    {
+        public int BookingID { get; set; }
+        public DateTime StartTid { get; set; }
+        public DateTime SlutTid { get; set; }
+
+        public Booking(DateTime startTid, DateTime slutTid)
+        {
+            if (startTid == default) throw new ArgumentException($"StartTid skal være udfuldt.");
+            StartTid = startTid;
+            SlutTid = slutTid;
+        }
+
+        public bool IsOverlapping(List<Booking> otherBookings)
+        {
+            var result = otherBookings.Any(a => a.StartTid >= StartTid && StartTid <= a.SlutTid);
+            return result;
+        }
+    }
+}
---
+++ BOOKING/Domain/Booking/IBooking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOOKING.Domain.Booking
+{
+    interface IBooking
+    {
+        int BookingID { get; set; }
+        DateTime StartTid { get; set; }
+        DateTime SlutTid { get; set; }
+    }
+}
