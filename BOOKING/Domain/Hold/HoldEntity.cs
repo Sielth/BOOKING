9cc5dc2dd7f53ad6a7b2df73be24a37fee3e2d7d
tbni26606 <trine.berg.nissen@gmail.com>
2021-09-21 11:39:50 +0200

    Domæne opsætning

---
+++ BOOKING/Domain/Hold/HoldEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOOKING.Domain.Hold
+{
+    public class HoldEntity
+    {
+        public int HoldID { get; }
+    }
+}
---
+++ BOOKING/Domain/Kalender/Kalender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOOKING.Domain.Kalender
+{
+    public class Kalender
+    {
+        public int KalenderID { get; }
+        public BookingVindue.BookingVindueEntity BookingVindueEntity { get; set; }
+        public Hold.HoldEntity HoldEntity { get; set; }
+    }
+}
