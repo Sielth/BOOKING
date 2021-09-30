b4ec20b5f4368a423841e9acf95a9e250745b538
Jon4dis <yuppst3r@gmail.com>
2021-09-30 09:46:13 +0200

    Start

---
+++ BOOKING/BOOKING.Persistence/Repositories/BookingRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BOOKING.Domain.Booking;
+
+namespace BOOKING.Persistence.Repositories
+{
+    class BookingRepository : IBookingEntity
+    {
+    }
+}
